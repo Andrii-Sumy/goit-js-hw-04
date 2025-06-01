a52c2756a89d1277f10b8d117b451514d11dbd18
Andrii Kovalenko <01birofe01@gmail.com>
2025-06-01 20:29:48 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,30 @@
+function filterArray(numbers, value) {
+    let massiv = [];
+    for (const numb of numbers) {
+        if (numb > value) {
+            massiv.push(numb);
+        }
+    }
+    return massiv;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
